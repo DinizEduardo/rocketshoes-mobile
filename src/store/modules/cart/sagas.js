4666c3ba526afa3e763dea5e58d86c617021cdd2
Eduardo Diniz <eduardo.diniz987@gmail.com>
2019-07-22 16:49:35 -0300

    Validação da quantidade de produtos em estoque

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,25 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 
+import { Alert } from 'react-native';
 import api from '../../../services/api';
-
 import { addToCartSuccess } from './actions';
 // import console = require('console');
 
 function* addToCart({ id }) {
+  const productIndex = yield select(state => state.cart.find(p => p.id === id));
+
+  const stockResponse = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stockResponse.data.amount;
+
+  const currentAmount = productIndex ? productIndex.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    Alert.alert('', 'Quantidade solicitada fora de estoque.');
+    return;
+  }
+
   const response = yield call(api.get, `/products/${id}`);
 
   yield put(addToCartSuccess(response.data));
